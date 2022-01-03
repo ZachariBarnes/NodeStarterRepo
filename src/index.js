ca67f1b1aacfa4403862f2e964e225a901543f1c
Zachari Barnes <ZachariBarnes@yahoo.com>
2022-01-03 11:35:22 -0600

    comply with eslint, adding common packages  and start express file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import got from 'got';
+import express from 'express';
+import logger from 'npmlog';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const myUrl = process.env.URL || 'http://localhost';
+const port = process.env.PORT || 3333;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 const testFunction = async () => {
   const { data } = await got.post('https://httpbin.org/anything', {
@@ -7,7 +22,9 @@ const testFunction = async () => {
     },
   }).json();
 
-  console.log(data);
+  logger.info(data);
   //= > {"hello": "world"}
 };
 testFunction();
+
+app.listen(port, () => logger.info(`App running at: ${myUrl}:${port}`));
